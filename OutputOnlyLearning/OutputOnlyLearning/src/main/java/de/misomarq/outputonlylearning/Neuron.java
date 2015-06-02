6d9fcf7af39bf6274c67ea943122de787e9976b4
David Marquant <david.marquant.93@gmail.com>
2015-06-02 23:49:55 +0200

    Created pom project for Neuronal network

---
+++ OutputOnlyLearning/OutputOnlyLearning/src/main/java/de/misomarq/outputonlylearning/Neuron.java
@@ -0,0 +1,44 @@
+package de.misomarq.outputonlylearning;
+
+/************************************************************************
+* \brief: a single Neuron class                                         *
+*																		*
+* (c) copyright by Jörn Fischer											*
+*                                                                       *																		* 
+* @autor: Prof.Dr.Jörn Fischer											*
+* @email: j.fischer@hs-mannheim.de										*
+*                                                                       *
+* @file : Neuron.java                                                   *
+*************************************************************************/
+
+public class Neuron {
+	public double output;
+	public double activity;
+	public double weight[];
+	public int numOfWeights;
+	
+	/**
+	 * @brief: Constructor to reserve memory
+	 */
+	public Neuron()
+	{
+		weight = new double[100];
+		this.numOfWeights = 30;
+	}
+	
+	/**
+	 * @brief: setter function to set the number of weights
+	 * @param numOfWeights
+	 */
+	public void setNumOfWeights(int numOfWeights)
+	{
+		//System.out.println(numOfWeights);
+		weight = new double[numOfWeights];
+		for (int t=0;t<numOfWeights;t++){
+			weight[t]=0;
+		}
+		this.numOfWeights = numOfWeights;
+	}
+	
+}
+
---
+++ OutputOnlyLearning/OutputOnlyLearning/src/main/java/de/misomarq/outputonlylearning/StockPricePrediction.java
@@ -0,0 +1,11 @@
+package de.misomarq.outputonlylearning;
+
+/**
+ *
+ * @author dmarquant
+ */
+public class StockPricePrediction {
+    public static void main(String []args) {
+        System.out.println("Hello, world!");
+    }
+}
